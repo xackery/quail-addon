94365d48666f2f4024719b52e0cf13333e6edddf
Xackery Xtal <xackery@gmail.com>
2023-07-21 17:29:51 -0700

    Add latest revamp

--- exporter/mesh_export.py
+++ exporter/mesh_export.py
@@ -1,5 +1,7 @@
 import bpy
 import os
+
+from mathutils import Vector
 from .material_export import material_export
 import bmesh
 
@@ -27,7 +29,6 @@ def mesh_object_export(quail_path: str, mesh_path: str, mesh_name: str, obj: bpy
     bm.from_mesh(mesh, face_normals=True, vertex_normals=True)
     if is_triangulate:
         bmesh.ops.triangulate(bm, faces=bm.faces)  # type: ignore
-    bm.faces.ensure_lookup_table()
     bm.normal_update()
 
     material_export(quail_path, mesh_path, mesh.materials)
@@ -38,33 +39,38 @@ def mesh_object_export(quail_path: str, mesh_path: str, mesh_name: str, obj: bpy
     tw = open("%s/triangle.txt" % mesh_path, "w")
     tw.write("index|flag|material_name\n")
 
-    mesh.uv_layers.active = mesh.uv_layers[0]
-    mesh.uv_layers.active_index = 0
-    mesh.uv_layers[0].active_render = True
-    uv = mesh.uv_layers[0].data
+    uv_layer = bm.loops.layers.uv.active if bm.loops.layers.uv.active else None
     flag_layer = bm.faces.layers.int.get("flag")  # type: ignore
     if flag_layer is None:
         flag_layer = bm.faces.layers.int.new("flag")  # type: ignore
 
     verts = {}
     print("found %d faces" % len(bm.faces))
+    bm.verts.index_update()
+    bm.verts.ensure_lookup_table()
+    bm.faces.ensure_lookup_table()
+    mesh.uv_layers.active = mesh.uv_layers[0]
+    mesh.uv_layers.active_index = 0
+    mesh.uv_layers[0].active_render = True
+
     for face in bm.faces:  # type: ignore
         if len(face.verts) != 3:
             print("skipping face with %d verts (want 3)" % len(face.verts))
             continue
         index_str = ""
-        # face = bm.faces[face.index]
         for loop in face.loops:
             vert = loop.vert
             index_str += "%d," % vert.index
-            line = ""
-            line += "%0.8f,%0.8f,%0.8f|" % (vert.co.x, vert.co.y, vert.co.z)
-            line += "%0.8f,%0.8f,%0.8f|" % (
+            vert_str = "%0.8f,%0.8f,%0.8f|" % (
+                vert.co.x, vert.co.y, vert.co.z)
+            vert_str += "%0.8f,%0.8f,%0.8f|" % (
                 vert.normal.x, vert.normal.y, vert.normal.z)
-            line += "%0.8f,%0.8f|" % (uv[loop.index].uv.x, uv[loop.index].uv.y)
-            line += "%0.8f,%0.8f|" % (0, 0)
-            line += "%d,%d,%d,%d\n" % (120, 120, 120, 255)
-            verts[vert.index] = line
+            vert_str += "%0.8f,%0.8f|" % (
+                loop[uv_layer].uv.x, loop[uv_layer].uv.y)
+            vert_str += "%0.8f,%0.8f|" % (0, 0)
+            vert_str += "%d,%d,%d,%d\n" % (120, 120, 120, 255)
+            verts[vert.index] = vert_str
+
         index_str = index_str[:-1]
         tw.write("%s|" % index_str)
         tw.write("%s|%s\n" %
